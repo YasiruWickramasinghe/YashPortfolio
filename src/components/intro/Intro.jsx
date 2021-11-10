e7438951d4bd187f874d615cac2ebc799f18f2a9
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2021-11-10 11:11:09 +0530

    Intro complete

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,20 @@
 import "./intro.scss"
+import {init} from 'ityped'
+import { useEffect, useRef } from "react"
 
 export default function Intro() {
+
+const textRef = useRef();    
+
+useEffect(()=>{
+    init(textRef.current, {
+        showCursor: true,
+        backDelay : 1500,
+        backSpeed:60,
+        strings: ['FullStack Developer', 'Tech Lead', 'Freelancer',  'Web Developer', 'Mobile Developer' ],
+     });
+},[])
+
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -12,7 +26,7 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi There, I'm</h2>
                     <h1>YASIRU WICKRAMASINGHE</h1>
-                    <h3>Software Engineer<span></span></h3>
+                    <h3> <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
                     <img src="assets/down.png" alt="" />
