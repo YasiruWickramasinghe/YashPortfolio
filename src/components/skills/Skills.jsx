1c54a5e434e71b686ba56cdbd15dc14746a37c5c
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2021-11-17 19:40:07 +0530

    stuff

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,10 +1,92 @@
 import "./skills.scss";
+import {DoubleArrow} from "@material-ui/icons";
 
 export default function Skills() {
 
     return (
         <div className="skills" id="skills">
-            <h1>Skills</h1>
+            <div className="container">
+
+                <h1>Skills</h1>
+
+                <div className="top">
+                    <h2>Programming Language and Frameworks</h2>
+                    <div className="item">
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                    </div>
+                </div>
+                <div className="center">
+                <h2>Programming Language and Frameworks</h2>
+                    <div className="item">
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                    </div>
+                </div>
+                <div className="bottom">
+                <h2>Programming Language and Frameworks</h2>
+                    <div className="item">
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                        <DoubleArrow className="icon"/>
+                    </div>
+                </div>
+
+            </div>
+            
         </div>
     )
 }
