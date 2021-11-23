c3e4511b43605280d659f5109fcb97cd9e355f6c
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2021-11-23 12:14:07 +0530

    skill section complete

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -14,153 +14,137 @@ export default function Skills() {
                         <hr/>
                         <div className="skillBlock">
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img className="icon" src="https://img.icons8.com/ios/50/1d809f/java-coffee-cup-logo--v1.png"/>
+                                <h4>JAVA</h4>
                             </div>  
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img className="icon" src="https://img.icons8.com/ios/50/1d809f/python.png"/>
+                                <h4>PYTHON</h4>
                             </div>
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img className="icon" src="https://img.icons8.com/ios/50/1d809f/javascript--v1.png"/>
+                                <h4>JAVA SCRIPT</h4>
                             </div>
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img className="icon" src="https://img.icons8.com/ios-glyphs/50/1d809f/react.png"/>
+                                <h4>REACT</h4>
                             </div>
                         </div>
                         <div className="skillBlock">
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img className="icon" src="https://img.icons8.com/windows/50/1d809f/nodejs.png"/>
+                                <h4>NODE JS</h4>
                             </div>  
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img className="icon" src="https://img.icons8.com/windows/50/1d809f/vuejs.png"/>
+                                <h4>VUE JS</h4>
                             </div>
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img className="icon" src="https://img.icons8.com/ios/50/1d809f/c-sharp-logo.png"/>
+                                <h4>C#</h4>
                             </div>
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img className="icon" src="https://img.icons8.com/material-outlined/50/1d809f/php.png"/>
+                                <h4>PHP</h4>
                             </div>
                         </div>
                         <div className="skillBlock">
                             <div className="skill">
-                                <DoubleArrow className="icon" />
+                                <img className="icon" src="https://img.icons8.com/material-outlined/50/1d809f/asp.png"/>
                                 <h4>Hello</h4>
                             </div>  
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img className="icon" src="https://img.icons8.com/windows/50/1d809f/bootstrap.png"/>
+                                <h4>Bootstrap</h4>
                             </div>
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img className="icon" src="https://img.icons8.com/windows/50/1d809f/mdb.png"/>
+                                <h4>MDB</h4>
                             </div>
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img className="icon" src="https://img.icons8.com/material-outlined/50/1d809f/django.png"/>
+                                <h4>DJANGO</h4>
                             </div>
                         </div>
                         <div className="skillBlock">
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                            <img className="icon" src="https://img.icons8.com/ios/50/1d809f/react-native--v1.png"/>
+                                <h4>React Native</h4>
                             </div>  
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img className="icon" src="https://img.icons8.com/ios/50/1d809f/flutter.png"/>
+                                <h4>FLUTTER</h4>
                             </div>
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img  className="icon" src="https://img.icons8.com/material-outlined/50/1d809f/kotlin.png"/>
+                                <h4>KOTLIN</h4>
                             </div>
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img className="icon" src="https://img.icons8.com/ios/50/1d809f/c.png"/>
+                                <h4>C</h4>
                             </div>
                         </div>    
                     </div>
                 </div>
                 <div className="item">
                     <div className="cardItem">
-                        <h2>Tools and Technologies</h2>
+                        <h2>Database and Technologies</h2>
                         <hr/>
                         <div className="skillBlock">
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img src="https://img.icons8.com/material-outlined/50/1d809f/sql.png"/>
+                                <h4>MS SQL</h4>
                             </div>  
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img src="https://img.icons8.com/ios/50/1d809f/mysql.png"/>
+                                <h4>MY SQL</h4>
                             </div>
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                            <img src="https://img.icons8.com/ios-glyphs/50/1d809f/windows-client.png"/>
+                                <h4>WINDOWS</h4>
                             </div>
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img src="https://img.icons8.com/ios/50/1d809f/linux.png"/>
+                                <h4>LINUX</h4>
                             </div>
                         </div>
                         <div className="skillBlock">
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img src="https://img.icons8.com/ios-filled/50/1d809f/git.png"/>
+                                <h4>GIT</h4>
                             </div>  
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img src="https://img.icons8.com/fluency-systems-regular/50/1d809f/circled-a--v2.png"/>
+                                <h4>AZURE BOARD</h4>
                             </div>
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                            <img src="https://img.icons8.com/ios-filled/50/1d809f/visual-studio-logo.png"/>
+                                <h4>VISUAL STUDIO</h4>
                             </div>
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img src="https://img.icons8.com/ios-filled/50/1d809f/java-eclipse.png"/>
+                                <h4>ECLIPSE IDE</h4>
                             </div>
                         </div>
                         <div className="skillBlock">
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img src="https://img.icons8.com/ios/50/1d809f/visual-studio-logo.png"/>
+                                <h4>VS CODE</h4>
                             </div>  
                             <div className="skill">
-                                <DoubleArrow className="icon" />
+                            <img src="https://img.icons8.com/ios/50/1d809f/android-studio--v2.png"/>
                                 <h4>Hello</h4>
                             </div>
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img src="https://img.icons8.com/windows/50/1d809f/netbeans.png"/>
+                                <h4>NET BEANS</h4>
                             </div>
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img src="https://img.icons8.com/ios-glyphs/50/1d809f/github.png"/>
+                                <h4>GIT HUB</h4>
                             </div>
                         </div>
                         <div className="skillBlock">
-                            <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
-                            </div>  
-                            <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
-                            </div>
-                            <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
-                            </div>
-                            <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
-                            </div>
                         </div> 
                     </div>
                 </div>
@@ -170,58 +154,40 @@ export default function Skills() {
                         <hr/>
                         <div className="skillBlock">
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img src="https://img.icons8.com/material-outlined/50/1d809f/adobe-photoshop.png"/>
+                                <h4>PHOTOSHOP</h4>
                             </div>  
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img src="https://img.icons8.com/ios/50/1d809f/adobe-illustrator--v1.png"/>
+                                <h4>ILLUSTRATOR</h4>
                             </div>
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img src="https://img.icons8.com/material-outlined/50/1d809f/adobe-xd.png"/>
+                                <h4>XD</h4>
                             </div>
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                            <img src="https://img.icons8.com/material-outlined/50/1d809f/adobe-premiere-pro.png"/>
+                                <h4>PREMIERE PRO</h4>
                             </div>
                         </div>
                         <div className="skillBlock">
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                <img src="https://img.icons8.com/ios-glyphs/50/1d809f/office-365.png"/>
+                                <h4>OFFICE 365</h4>
                             </div>  
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
+                                < img src="https://img.icons8.com/ios/50/1d809f/teamviewer.png"/>
+                                <h4>TEAM VIEWER</h4>
                             </div>
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
-                            </div>
+                            </div>  
                             <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
                             </div>
                         </div>
                         <div className="skillBlock">
-                            <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
-                            </div>  
-                            <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
-                            </div>
-                            <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
-                            </div>
-                            <div className="skill">
-                                <DoubleArrow className="icon" />
-                                <h4>Hello</h4>
-                            </div>
                         </div>
+                        <div className="skillBlock">
+                        </div> 
 
                     </div>
                 </div>
